6dd445646385189f074c30cf3115b6f3844b36d3
Lourens KOEKEMOER <lkoekemo@e4r17p10.wethinkcode.co.za>
2017-11-15 17:53:41 +0200

    0

--- 00_how_to_docker/setup.sh
+++ 00_how_to_docker/setup.sh
@@ -2,37 +2,74 @@
 # NOC="\033[0m"
 # BLUE="\033[0;34m"
 # WHITE="\033[1;37m"
+# sh -c "$(curl -fsSL https://raw.githubusercontent.com/Tolsadus/42homebrewfix/master/install.sh)"
+# brew install docker
+# brew install docker-machine
+echo "01"
 sh 01
+echo "02"
 sh 02
+echo "03"
 source ./03
+echo "04"
 sh 04
+echo "05"
 sh 05
+echo "06"
 sh 06
+echo "07"
 sh 07
+echo "08"
 sh 08
+echo "09"
 sh 09
+echo "10"
 sh 10
+echo "11"
 sh 11
+echo "12"
 sh 12
+echo "13"
 sh 13
+echo "14"
 sh 14
+echo "15"
 sh 15
+echo "16"
 sh 16
+echo "17"
 sh 17
+echo "18"
 sh 18
+echo "19"
 sh 19
+echo "20"
 sh 20
+echo "21"
 sh 21
+echo "22"
 sh 22
+echo "23"
 sh 23
+echo "24"
 sh 24
+echo "25"
 sh 25
-sh 26
+echo "26"
+sh 26       #ISSUE
+echo "27"
 sh 27
+echo "28"
 sh 28
+echo "29"
 sh 29
+echo "30"
 sh 30
-sh 31
-sh 32
-sh 33
-sh 34
+echo "31"
+# sh 31
+# echo "32"
+# sh 32
+# echo "33"
+# sh 33
+# echo "34"
+# sh 34
